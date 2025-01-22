1da42333b7ff7581ba4889f2209260cef1d3ab29
E1300299 <e-aenn@gft.com>
2025-01-22 12:56:18 +0100

    First Commit

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ app.use('/api/users', userRoutes);
 app.use('/api/product', productRoutes);
 
 // Avvio del server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server in ascolto sulla porta ${PORT}`);
 });
