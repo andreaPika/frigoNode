da328b2a7b35015da8b73a7ec676af6294bd19cf
E1300299 <e-aenn@gft.com>
2025-01-23 09:47:19 +0100

    First Commit

--- models/product.js
+++ models/product.js
@@ -4,6 +4,7 @@ const productSchema = new mongoose.Schema({
   name: { type: String, required: true },
   category: { type: mongoose.Schema.Types.ObjectId, ref: 'Category' },
   quantity: { type: Number, required: true, min: 0 },
+  unit: { type: String, required: true },
   expiryDate: { type: Date, required: true },
   fridgePosition: { type: mongoose.Schema.Types.ObjectId, ref: 'FridgePosition' }, // Ad esempio: "Ripiano 2, destra"
 });
